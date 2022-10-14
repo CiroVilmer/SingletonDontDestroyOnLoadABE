fb10ae64fbc4c9c12b62a1833eae4add02e301be
CiroVilmer <101563425+CiroVilmer@users.noreply.github.com>
2022-10-14 10:38:24 -0300

    Initial commit

---
+++ Assets/PlayerInput.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public SceneChanger sceneChangerScript;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            sceneChangerScript.CambiarDeEscenaSecuencialmente();
+        }
+    }
+}
---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public int puntaje;
+    public static PlayerScript instance;
+
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
